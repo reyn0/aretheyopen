2858b619411a583e051ebcc96116037d387320bb
Reynaldi Hadianto <reynaldi.hadianto@gmail.com>
2011-06-20 22:51:02 +1000

    added new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aretheyopen::Application.config.secret_token = '564e21b73492b14ca5eb689a6ffe10104ea21885af93c28c0591e4b9f9509d512e0f0056a1cd89592e1658b24398c75dd89bf7eefbda60e25406b3ccae2c9139'
